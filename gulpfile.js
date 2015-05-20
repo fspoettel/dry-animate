f2bb7ccef26fe366b59e50353180aca3dfc80e1b
felics <fspoettel@gmail.com>
2015-05-20 13:03:39 +0200

    Add package.json, update deps, don't use ES6 in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,11 @@
 
 "use strict";
 
-let gulp = require("gulp"),
+var gulp = require("gulp"),
     sass = require("gulp-sass");
 
 gulp.task("default", function() {
   gulp.src("./scss/_dry-animate.scss")
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest("./"))
+    .pipe(gulp.dest("./"));
 });
