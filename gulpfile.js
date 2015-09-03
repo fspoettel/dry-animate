c4477cd9b536c1b1a0ee84dcbb7b29a79e402e4f
felics <fspoettel@gmail.com>
2015-09-03 15:43:37 +0200

    Syntax in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,20 @@
 // @tasks: parse-css, build-tests, clean
 ///////////////////////////////
 
-var gulp     = require("gulp"),
-    rubySass = require("gulp-ruby-sass"),
-    sass     = require("gulp-sass");
+var gulp     = require('gulp');
+var rubySass = require('gulp-ruby-sass');
+var sass     = require('gulp-sass');
 
-gulp.task("test_ruby", function() {
-  rubySass("./_test/src")
-    .on('error', function (err) {
+gulp.task('test_ruby', function() {
+  rubySass('./_test/src')
+    .on('error', function(err) {
       console.error('Error!', err.message);
     })
-    .pipe(gulp.dest("./_test/build/ruby"));
+    .pipe(gulp.dest('./_test/build/ruby'));
 });
 
-gulp.task("test", function() {
-  gulp.src("./_test/src/*.scss")
+gulp.task('test', function() {
+  gulp.src('./_test/src/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest("./_test/build/libsass"));
+    .pipe(gulp.dest('./_test/build/libsass'));
 });
